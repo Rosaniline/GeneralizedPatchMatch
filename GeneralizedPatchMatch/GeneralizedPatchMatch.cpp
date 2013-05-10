1c6e13f086cc851239e5d53b5de4d706171c4a2b
Rosaniline <xup6qup3@gmail.com>
2013-05-10 16:18:40 +0800

    init git

--- GeneralizedPatchMatch/GeneralizedPatchMatch.cpp
+++ GeneralizedPatchMatch/GeneralizedPatchMatch.cpp
@@ -1,18 +1,9 @@
 //
-//  main.cpp
+//  GeneralizedPatchMatch.cpp
 //  GeneralizedPatchMatch
 //
 //  Created by Rosani Lin on 13/5/10.
 //  Copyright (c) 2013年 Rosani Lin. All rights reserved.
 //
 
-#include <iostream>
 #include "GeneralizedPatchMatch.h"
-
-int main(int argc, const char * argv[])
-{
-
-
-    return 0;
-}
-
---
+++ GeneralizedPatchMatch/GeneralizedPatchMatch.h
@@ -0,0 +1,26 @@
+//
+//  GeneralizedPatchMatch.h
+//  GeneralizedPatchMatch
+//
+//  Created by Rosani Lin on 13/5/10.
+//  Copyright (c) 2013年 Rosani Lin. All rights reserved.
+//
+
+#ifndef __GeneralizedPatchMatch__GeneralizedPatchMatch__
+#define __GeneralizedPatchMatch__GeneralizedPatchMatch__
+
+#include <iostream>
+#include "Rosaniline.h"
+
+
+class GeneralizedPatchMatch {
+    
+public:
+    
+    
+private:
+
+};
+
+
+#endif /* defined(__GeneralizedPatchMatch__GeneralizedPatchMatch__) */
