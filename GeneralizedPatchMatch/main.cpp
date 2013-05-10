26b7b90f2cbcfd34bb98b5aff0e4cad43c3f0e9b
Rosaniline <xup6qup3@gmail.com>
2013-05-10 16:12:44 +0800

    Initial Commit

---
+++ GeneralizedPatchMatch/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GeneralizedPatchMatch
+//
+//  Created by Rosani Lin on 13/5/10.
+//  Copyright (c) 2013年 Rosani Lin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
